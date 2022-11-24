29fc8bf54b208f3005cf1a671e87937bbbcacf50
sifatullahsu <personal.sifat@gmail.com>
2022-11-24 12:38:18 +0600

    mongodb setup & get all users

--- index.js
+++ index.js
@@ -1,11 +1,42 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+require('dotenv').config();
 
 const port = process.env.PORT || 5000;
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+const user = process.env.DB_USER;
+const pass = process.env.DB_PASS;
+
+const uri = `mongodb+srv://${user}:${pass}@cluster0.rbe1w.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1
+});
+
+const run = async () => {
+  try {
+    const db = client.db('antique-watches');
+    const usersCollection = db.collection('users');
+
+    app.get('/users', async (req, res) => {
+      const query = {}
+      const result = await usersCollection.find(query).toArray();
+
+      res.send(result);
+    })
+
+  }
+  finally {
+
+  }
+}
+run().catch(err => console.error(err));
 
 
 app.get('/', (req, res) => {
